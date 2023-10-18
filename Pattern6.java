6da2d73b73348baa7f0b0ba870f5d4062960b6e8
Sandhya1709 <148322557+Sandhya1709@users.noreply.github.com>
2023-10-18 16:41:24 +0530

    Update Pattern6.java

--- Pattern6.java
+++ Pattern6.java
@@ -1,12 +1,13 @@
 public class Pattern6 {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j--){
-                System.out.println(" ");
+            for(int j=i;j<=5;j++){
+                System.out.print(" ");
             }
             for(int k=1;k<=5;k++){
-                System.out.println("*");
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
     
